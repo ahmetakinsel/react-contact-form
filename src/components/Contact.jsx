5b7bc6ef4b2c2599c4d59d078952ecb043bbf362
ahmet92akinsel <ahmet.akinsel@gmail.com>
2022-05-27 00:54:41 +0300

    contact form layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,18 +3,36 @@ import emailjs from "@emailjs/browser";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import styled from "styled-components";
+import img from "./img/img.jpg";
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
+  padding: 50px;
 `;
 
-const ImageWrapper = styled.div``;
+const ImageWrapper = styled.div`
+  display: flex;
+  padding: 5px;
+`;
 
 const Form = styled.form`
   display: flex;
+`;
+
+const FormFields = styled.div`
+  display: flex;
   justify-content: center;
   flex-direction: column;
+  padding: 5px;
+`;
+
+const FieldWrapper = styled.div``;
+
+const LabelWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 10px 10px 10px 0px;
 `;
 
 const InputLabel = styled.label`
@@ -23,18 +41,19 @@ const InputLabel = styled.label`
 `;
 
 const Input = styled.input`
-  width: 615px;
+  width: 550px;
   height: 35px;
 `;
 
-const Button = styled.input`
-  width: 615px;
+const Button = styled.button`
+  width: 558px;
   height: 35px;
 `;
 
 const TextField = styled.textarea`
-  width: 615px;
-  height: 300px;
+  width: 550px;
+  height: 250px;
+  margin-bottom: 25px;
 `;
 
 const Contact = () => {
@@ -72,38 +91,61 @@ const Contact = () => {
       );
   };
   return (
-    <Container>
-      <ImageWrapper>
-        <img src="public/assets/img.jpg" alt="img" />
-      </ImageWrapper>
-      <Form ref={form} onSubmit={sendEmail}>
-        <InputLabel>Name</InputLabel>
-        <Input
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-          name="user_name"
-        />
-        <InputLabel>Subject</InputLabel>
-        <Input
-          onChange={(e) => setSubject(e.target.value)}
-          type="text"
-          name="subject"
-        />
-        <InputLabel>Email</InputLabel>
-        <Input
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          name="user_email"
-        />
-        <InputLabel>Message</InputLabel>
-        <TextField
-          onChange={(e) => setMessage(e.target.value)}
-          name="message"
-        />
-        <Button type="submit" value="Send" />
-        <ToastContainer />
-      </Form>
-    </Container>
+    <>
+      <Container>
+        <ImageWrapper>
+          <img src={img} alt="img" width="615px" height="100%" />
+        </ImageWrapper>
+        <Form ref={form}>
+          <FormFields>
+            <h1>Contact</h1>
+            <FieldWrapper>
+              <LabelWrapper>
+                <InputLabel>Name</InputLabel>
+              </LabelWrapper>
+              <Input
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                name="user_name"
+              />
+            </FieldWrapper>
+            <FieldWrapper>
+              <LabelWrapper>
+                <InputLabel>Subject</InputLabel>
+              </LabelWrapper>
+              <Input
+                onChange={(e) => setSubject(e.target.value)}
+                type="text"
+                name="subject"
+              />
+            </FieldWrapper>
+            <FieldWrapper>
+              <LabelWrapper>
+                <InputLabel>Email</InputLabel>
+              </LabelWrapper>
+              <Input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                name="user_email"
+              />
+            </FieldWrapper>
+            <FieldWrapper>
+              <LabelWrapper>
+                <InputLabel>Message</InputLabel>
+              </LabelWrapper>
+              <TextField
+                onChange={(e) => setMessage(e.target.value)}
+                name="message"
+              />
+            </FieldWrapper>
+            <Button type="submit" onClick={sendEmail}>
+              Send
+            </Button>
+            <ToastContainer />
+          </FormFields>
+        </Form>
+      </Container>
+    </>
   );
 };
 export default Contact;
