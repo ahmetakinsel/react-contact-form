bbfc0398aba0ccbd9e1ea5201db5dc9ff6ac25d8
ahmet92akinsel <ahmet.akinsel@gmail.com>
2022-05-26 17:55:59 +0300

    email integration

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,6 +4,27 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const ContactForm = () => {
+  /*
+https://www.emailjs.com/docs/examples/reactjs/
+https://www.emailjs.com/docs/
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+*/
+
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
